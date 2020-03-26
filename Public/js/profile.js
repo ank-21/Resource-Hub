9738c180421459aa70164bf36633e4373517e466
anuva04 <anuvacotton2016@gmail.com>
2020-03-26 18:03:27 +0530

    modal and bookcard added

--- Public/js/profile.js
+++ Public/js/profile.js
@@ -350,3 +350,25 @@ $(function() {
   }
 
 }); // end of document ready
+
+//Get modal element
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementById('closeBtn');
+
+modalBtn.addEventListener('click', openModal);
+function openModal(){
+  modal.style.display = 'block';
+}
+
+closeBtn.addEventListener('click', closeModal);
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+window.addEventListener('click', clickOutside);
+function clickOutside(e){
+  if(e.target == modal1){
+    modal.style.display = 'none';
+  }
+}
