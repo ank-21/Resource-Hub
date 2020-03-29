9e288f03ece1132731c0a73da4f0c3c53b38a086
anuva04 <anuvacotton2016@gmail.com>
2020-03-29 18:33:19 +0530

    updated navbar in branch-cards2.html

--- Public/js/branch-cards2.js
+++ Public/js/branch-cards2.js
@@ -1,10 +1,11 @@
-var smallnavbar = document.getElementById('smallnavbar');
-var navtoggle = document.getElementById('navtoggle');
-navtoggle.addEventListener('click', function(){
-    // if(smallnavbar.style.display == 'none'){
-        smallnavbar.style.display == 'block';
-    // }
-    // else {
-    //     smallnavbar.style.display == 'none';
-    // }
+var smallnavbar = document.getElementById("smallnavbar");
+var navtoggle = document.getElementById("navtoggle");
+navtoggle.addEventListener("click", function(){
+    if(smallnavbar.style.display == 'none'){
+        smallnavbar.style.display = "block";
+    }
+    else {
+        smallnavbar.style.display = "none";
+        smallnavbar.style.transition = "1s ease-in-out"
+    }
 })
