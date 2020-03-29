a26a7189a550b50090aa6469268e3041de94d9a4
anuva04 <anuvacotton2016@gmail.com>
2020-03-29 18:02:27 +0530

    updated add notes

---
+++ Public/js/branch-cards2.js
@@ -0,0 +1,10 @@
+var smallnavbar = document.getElementById('smallnavbar');
+var navtoggle = document.getElementById('navtoggle');
+navtoggle.addEventListener('click', function(){
+    // if(smallnavbar.style.display == 'none'){
+        smallnavbar.style.display == 'block';
+    // }
+    // else {
+    //     smallnavbar.style.display == 'none';
+    // }
+})
