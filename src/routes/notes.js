c6bea639e10334b6bd5a6be9f0cd314743ce4109
ank-21 <ankitSrivastava21598@gmail.com>
2020-03-30 23:26:53 +0530

    particular branch route made and anchor tag used

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -12,10 +12,33 @@ router.get('/branch',(req,res)=>{
     res.render('branch');
 });
 
-router.get('/branch/semester',(req,res)=>{
-    
+router.get('/branch/semester', async(req,res)=>{
+    const branchname = req.query.branch;
+    console.log(branchname);
+    const selectedNotesByBranch = await Notes.find({branch:branchname});
+    console.log("all branch note",selectedNotesByBranch);
+    //it will give an array of object
+    const allSemDetails = [];
+    for(let i=1;i<=8;i++){
+        eachsemvariable = selectedNotesByBranch.filter(note => {
+            return note.semester == `${i}`;
+        });
+        console.log("each sem details",eachsemvariable);
+        
+        if(eachsemvariable.length==0){
+            allSemDetails.push('0');
+        }else{
+            allSemDetails.push(eachsemvariable);
+        }
+    }
+    res.send(allSemDetails[1]);   //as an array so start with 0
+    //allsemdetails has every details of a particular branch now
+    res.render('semester');
+
 })
 
-
+router.get('/branch/semester/notes',(req,res)=>{
+    
+})
 
 module.exports = router;
