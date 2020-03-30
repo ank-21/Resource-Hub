06ac56cc4c879703fc4a20d056201ced32c34b9b
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-03-30 16:19:42 +0530

    Modified branchpage and sempage

--- Public/js/branch-cards2.js
+++ Public/js/branch-cards2.js
@@ -2,7 +2,8 @@ var smallnavbar = document.getElementById("smallnavbar");
 var navtoggle = document.getElementById("navtoggle");
 navtoggle.addEventListener("click", function(){
     if(smallnavbar.style.display == 'none'){
-        smallnavbar.style.display = "block";
+        smallnavbar.style.display = "flex";
+        smallnavbar.style.justifyContent ="center";
     }
     else {
         smallnavbar.style.display = "none";
