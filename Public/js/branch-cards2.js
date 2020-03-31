f495e47aa1b3900ccac0f95db729c26d78dbba6e
anuva04 <anuvacotton2016@gmail.com>
2020-03-31 21:22:57 +0530

    modified navbar

--- Public/js/branch-cards2.js
+++ Public/js/branch-cards2.js
@@ -10,3 +10,20 @@ navtoggle.addEventListener("click", function(){
         smallnavbar.style.transition = "1s ease-in-out"
     }
 })
+
+window.onscroll = function() {myfunction()};
+
+// Get the navbar
+var navbar = document.getElementsByClassName("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar[0].offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myfunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar[0].classList.add("sticky");
+  } else {
+    navbar[0].classList.remove("sticky");
+  }
+}
