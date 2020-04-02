63c01e15c693900134c7f7d991aa3bb0a66c73d8
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-04-02 22:40:30 +0530

    process.cwd()

--- src/app.js
+++ src/app.js
@@ -77,8 +77,11 @@ app.use((req,res,next)=>{
 
 
 //for static page
-const publicDirectoryPath = path.join(__dirname,'../public');
-app.use(express.static(publicDirectoryPath));
+// const publicDirectoryPath = path.join(__dirname,'../public');
+// app.use(express.static(publicDirectoryPath));
+
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD,'public')));
 
 //for Routes
 app.use('/',indexRouter);
