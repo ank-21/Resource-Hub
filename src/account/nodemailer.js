30256ccf973d867b2cd9b741adfac06b3701ce29
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-06-25 21:56:01 +0530

    Fix:Index page modified and mail created

--- src/account/nodemailer.js
+++ src/account/nodemailer.js
@@ -7,14 +7,14 @@ const contactUs = (data) => {
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: keys.mail.email,           //email id
-      pass: keys.mail.pass           //my gmail password
+      user: keys.helpMail.email,           //email id
+      pass: keys.helpMail.pass           //my gmail password
     },
     pool: true
   });
   
   var mailOptions = {
-    from: keys.mail.email,
+    from: keys.helpMail.email,
     to: `${data.email}`,
     subject:`Resource Hub - ${data.subject}`,
     html:`<p>Hii ${data.name},<br>"your message" - ${data.message} has been recieved and we will solve this issue soon!!</p>`
@@ -35,14 +35,14 @@ const contactAdmin = (data) => {
   var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: keys.mail.email,           //email id
-        pass: keys.mail.pass           //my gmail password
+        user: keys.helpMail.email,           //email id
+        pass: keys.helpMail.pass           //my gmail password
       },
       pool: true
     });
     
     var mailOptions = {
-      from: keys.mail.email,
+      from: keys.helpMail.email,
       to: 'ankitsrivastava21345@gmail.com',
       subject:`Resource Hub User Report`,
       html:`A new Report has been issued.<p>Name - ${data.name}</p><p>Email Id - ${data.email}</p><p>Subject - ${data.subject}</p><p>Message - ${data.message}</p><p>Is Image - ${data.isImg}</p>`};
@@ -62,14 +62,14 @@ const sendCorrectionMail = (data) => {
   var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: keys.mail.email,           //email id
-        pass: keys.mail.pass           //my gmail password
+        user: keys.helpMail.email,           //email id
+        pass: keys.mahelpMailil.pass           //my gmail password
       },
       pool: true
     });
     
     var mailOptions = {
-      from: keys.mail.email,
+      from: keys.helpMail.email,
       to: `${data.email}`,
       subject:`Resource Hub - Inappropriate Credentials`,
       html:`<p>Hii ${data.name},<br>Your account contains information (some inappropriate words) which are against the rules of Resouce Hub. Kindly, change them within the next 6 hours or else we'll have to remove your account </p>`
@@ -90,14 +90,14 @@ const reportsMail = (data) => {
   var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: keys.mail.email,           //email id
-        pass: keys.mail.pass           //my gmail password
+        user: keys.helpMail.email,           //email id
+        pass: keys.helpMail.pass           //my gmail password
       },
       pool: true
     });
     
     var mailOptions = {
-      from: keys.mail.email,
+      from: keys.helpMail.email,
       to: `${data.email}`,
       subject:`Resource Hub - ${data.noteType} Reported Inappropriate`,
       html:`<p>Hii ${data.name},<br>your ${data.noteType} of branch ${data.branch} and semester ${data.semester} has been reported more than 3 times. So you should check it and remove it if it is inappropriate, otherwise we have to remove it and you will also have to face some decrement in your rating.</p> <p>If you are having some issue then feel free to contact us through the contact us section at <a href="http://18.191.249.98:3000/#contact">http://18.191.249.98:3000/#contact</a> after logging in</p>`
@@ -117,14 +117,14 @@ const reportsMail = (data) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          user: keys.mail.email,           //email id
-          pass: keys.mail.pass           //my gmail password
+          user: keys.helpMail.email,           //email id
+          pass: keys.helpMail.pass           //my gmail password
         },
         pool: true
       });
       
       var mailOptions = {
-        from: keys.mail.email,
+        from: keys.helpMail.email,
         to: `${data.email}`,
         subject:`Resource Hub - Deleted Inappropriate ${data.noteType}`,
         html:`<p>Hii ${data.name},<br>your ${data.noteType} of branch ${data.branch} and semester ${data.semester} has been reported more than 5 times. So it is deleted automatically, and you also have some decrement in your rating.<br>We have warned you earlier too but still if you are having some issue then feel free to contact us through the contact us section at <a href="http://18.191.249.98:3000/#contact">http://18.191.249.98:3000/#contact</a> after logging in.</p>`
